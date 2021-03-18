90cfb10e746b5bb3fbdf8255a564ecd695cf73ae
bobtal <talevski.boban@gmail.com>
2021-03-19 00:34:32 +0100

    Implements the functionality of adding a geofence and adding a reminder in the database

---
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofencingConstants.kt
@@ -0,0 +1,6 @@
+package com.udacity.project4.locationreminders.geofence
+
+object GeofencingConstants {
+    const val ACTION_GEOFENCE_EVENT = "com.udacity.project4.ACTION_GEOFENCE_EVENT"
+    const val GEOFENCE_RADIUS_IN_METERS = 100f
+}
--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -1,14 +1,28 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSaveReminderBinding
+import com.udacity.project4.locationreminders.geofence.GeofenceBroadcastReceiver
+import com.udacity.project4.locationreminders.geofence.GeofencingConstants.ACTION_GEOFENCE_EVENT
+import com.udacity.project4.locationreminders.geofence.GeofencingConstants.GEOFENCE_RADIUS_IN_METERS
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
@@ -31,6 +45,7 @@ class SaveReminderFragment : BaseFragment() {
         return binding.root
     }
 
+    @SuppressLint("MissingPermission")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = this
@@ -42,15 +57,60 @@ class SaveReminderFragment : BaseFragment() {
 
         binding.saveReminder.setOnClickListener {
             val title = _viewModel.reminderTitle.value
-            val description = _viewModel.reminderDescription
+            val description = _viewModel.reminderDescription.value
             val location = _viewModel.reminderSelectedLocationStr.value
-            val latitude = _viewModel.latitude
+            val latitude = _viewModel.latitude.value
             val longitude = _viewModel.longitude.value
 
-//            TODO: use the user entered reminder details to:
+//            COMPLETED: use the user entered reminder details to:
 //             1) add a geofencing request
 //             2) save the reminder to the local db
+
+            val geofence = getGeofence(title, latitude, longitude)
+            val geofencingRequest = getGeofencingRequest(geofence)
+            val geofencingClient = LocationServices.getGeofencingClient(context!!)
+            geofencingClient.addGeofences(geofencingRequest, getPendingIntent())
+
+            val reminder = ReminderDataItem(
+                title, description, location, latitude, longitude)
+            _viewModel.saveReminder(reminder)
+
         }
+
+
+    }
+
+    // Get a geofencing request
+    private fun getGeofencingRequest(geofence: Geofence?): GeofencingRequest? {
+        val geofencingRequest = GeofencingRequest.Builder()
+            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+            .addGeofence(geofence)
+            .build()
+        return geofencingRequest
+    }
+
+    // Get a geofence
+    private fun getGeofence(
+        title: String?,
+        latitude: Double?,
+        longitude: Double?
+    ): Geofence? {
+        val geofence = Geofence.Builder()
+            .setRequestId(title)
+            .setCircularRegion(latitude!!, longitude!!, GEOFENCE_RADIUS_IN_METERS)
+            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+            .setExpirationDuration(Geofence.NEVER_EXPIRE)
+            .build()
+        return geofence
+    }
+
+    // Get a PendingIntent for the Broadcast Receiver that handles geofence transitions.
+    fun getPendingIntent() : PendingIntent {
+        val intent = Intent(context!!, GeofenceBroadcastReceiver::class.java)
+        intent.action = ACTION_GEOFENCE_EVENT
+        val geofencePendingIntent = PendingIntent.getBroadcast(
+            context!!, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        return geofencePendingIntent
     }
 
     override fun onDestroy() {
